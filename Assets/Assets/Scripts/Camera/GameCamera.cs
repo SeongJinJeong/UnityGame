39068c0ee87b934e38078d6bc9a303d995b3f6ef
SeongJin Jeong <jsj0718tjdwl@naver.com>
2022-12-10 22:34:08 +0900

    Commit

---
+++ Assets/Assets/Scripts/Camera/GameCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    GameObject Player = null;
+    Vector3 PlayerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPos = GameObject.FindGameObjectWithTag
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
